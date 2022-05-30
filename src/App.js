7757335b782b4c5a0565346946ebacf04fcad7c4
hossenmaruf <severusmaruf740@gmail.com>
2022-05-31 01:52:09 +0600

    Country-Ap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Countries from './components/Countries';
+import './App.css'
+import Search from './components/Search';
+// import Country from './components/Country';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const url = 'https://restcountries.com/v3.1/all';
+
+
+const App = () => {
+
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [filterCountries, setFilterCountries] = useState (countries) ;
+
+  const fetchData = async (url) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setFilterCountries(data);
+      setLoading(false);
+      setError(null);
+        
+
+
+    }
+
+    catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+
+
+
+
+  };
+
+
+
+
+
+  useEffect(() => {
+
+    fetchData(url)
+
+
+
+  }, [url])
+
+     const handleRemoveCounytry = (name) => {
+       const filter = filterCountries.filter((country) => country.name.common !== name );
+       setFilterCountries(filter) ;
+     }
+
+     const handleSearch = (searchValue) => {
+         let value = searchValue.toLowerCase() ;
+         const newCountries = countries.filter((country) => {
+           const countryName = country.name.common.toLowerCase() ;
+           return countryName.startsWith(value) ;
+         })
+         setFilterCountries(newCountries)
+
+     }
+  
+   return (
+     <>
+     
+        <h1> Country App  </h1>
+
+         <Search onSearch = {handleSearch} /> 
+      
+       
+        {isLoading && <h2>Loading... </h2>}
+        {error && <h2> {error.message} </h2>  }
+
+        {countries && <Countries countries = {filterCountries}
+        
+          onRemoveCountry = {handleRemoveCounytry}
+          
+        /> }
+     
+     
+     </>
+
+
+
+
+
+   )
+
+
+
+
+
+
+
 }
 
-export default App;
+export default App
